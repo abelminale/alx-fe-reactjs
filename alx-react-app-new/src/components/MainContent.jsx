2e27e6d3663d5ef45a832faa45b108180cc0c6be
Abel Berihun <Abel.Berihun@dar.com>
2024-08-17 11:52:19 +0300

    adding alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+const MainContent = () => {
+    return ( <main>
+        <p style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>I love to visit New York, Paris, and Tokyo.</p>
+      </main> );
+}
+ 
+export default MainContent;
