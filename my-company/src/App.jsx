f243657c3c8d5ca62cb44f2a7c1bb6a04616a92d
Abel Berihun <Abel.Berihun@dar.com>
2024-08-17 12:31:26 +0300

    my-company web site

---
+++ my-company/src/App.jsx
@@ -0,0 +1,28 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Footer />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#eee', textAlign: 'center' }}>
+      <p>&copy; 2024 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
