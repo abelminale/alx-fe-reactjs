eaccfc52da8d1a048f41b88d31d7cc2b2dbae75a
Abel Berihun <Abel.Berihun@dar.com>
2024-08-24 12:07:53 +0300

    Implementing Advanced Search and Filtering Capabilities adjust recipelist

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for routing
 import useRecipeStore from '../recipeStore';
 import SearchBar from './SearchBar';
 import IngredientFilter from './IngredientFilter';
@@ -21,7 +22,9 @@ const RecipeList = () => {
       {recipes.length > 0 ? (
         recipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> {/* Link to recipe details */}
+            </h3>
             <p>{recipe.description}</p>
           </div>
         ))
