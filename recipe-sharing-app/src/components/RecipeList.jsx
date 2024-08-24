b60459d9ab6bb36ce237ab1535e26e0f941c775c
Abel Berihun <Abel.Berihun@dar.com>
2024-08-24 11:43:26 +0300

    recipe-sharing-app

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import useRecipeStore from '../recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+
+
+    //git commit not working 
+    <div>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes available</p>
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
