5884cd03ea78bd6971cb8d3798cd8c496dc07ff2
Abel Berihun <Abel.Berihun@dar.com>
2024-08-24 11:04:36 +0300

    Recipe sharing app

--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <footer>
-             <p style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>© 2023 City Lovers</p>
-        </footer>
-     );
-}
- 
-export default Footer;
--- alx-react-app-props/src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-const MainContent = () => {
-    return ( <main>
-        <p style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>I love to visit New York, Paris, and Tokyo.</p>
-      </main> );
-}
- 
-export default MainContent;
