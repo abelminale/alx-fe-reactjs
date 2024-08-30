c12c36ade0cda03825a9b27f80ef289b58f3f073
Abel Minale Berihun <39369863+abelminale@users.noreply.github.com>
2024-08-30 13:35:50 +0000

    advanced router react adjsuting app jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
@@ -17,7 +17,8 @@ function App() {
           path="/profile/*"
           element={<ProtectedRoute element={<Profile />} isAuthenticated={isAuthenticated} />}
         />
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        {/* Update the dynamic route path to use "/blog/:id" */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
