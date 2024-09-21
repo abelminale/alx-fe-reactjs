362c34e82c38e5af5f0b2b4869e50915225cd78a
Abel Minale Berihun <39369863+abelminale@users.noreply.github.com>
2024-09-21 09:31:40 +0000

    minRepos

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -6,12 +6,12 @@ export const fetchUserData = async (username) => {
   return response.data;
 };
 
-export const searchUsers = async ({ username, location, repos }) => {
+export const searchUsers = async ({ username, location, minRepos }) => {
   const queryParts = [];
   if (username) queryParts.push(username);
   if (location) queryParts.push(`location:${location}`);
-  if (repos) queryParts.push(`repos:>${repos}`);
-  
+  if (minRepos) queryParts.push(`repos:>${minRepos}`);
+
   const query = queryParts.join(' ');
   const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query)}`);
   return response.data;
