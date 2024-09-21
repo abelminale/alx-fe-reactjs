a54ff5fb75a419aa7ee5e0cd9c6995d4f05ce7d6
Abel Minale Berihun <39369863+abelminale@users.noreply.github.com>
2024-09-21 07:59:10 +0000

    receipe-sharing-app

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="text-blue-500">
+      Tailwind CSS is working!
+    </div>
+  );
+}
+export default App;
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
