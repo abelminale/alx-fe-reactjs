4991271a7f340839d4273fd0bf7c18d38e0d3cd8
Abel Minale Berihun <39369863+abelminale@users.noreply.github.com>
2024-09-21 08:58:30 +0000

    adjsuting service.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,25 @@
 // src/services/githubService.js
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const BASE_URL = 'https://api.github.com';
 
+// Fetch user data by username
+export const fetchUserData = async (username) => {
+  const url = `${BASE_URL}/users/${username}`;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching user data');
+  }
+};
+
+// Advanced search for users by multiple criteria
 export const searchUsers = async ({ username, location, repos }) => {
-  const query = `q=${username ? `${username}+` : ''}${location ? `location:${location}+` : ''}${repos ? `repos:>${repos}` : ''}`;
-  const url = `${BASE_URL}?${query}`;
+  const query = `q=${username ? `${username}+` : ''}${location ? `location:${location}+` : ''}${
+    repos ? `repos:>${repos}` : ''
+  }`;
+  const url = `${BASE_URL}/search/users?${query}`;
 
   try {
     const response = await axios.get(url);
